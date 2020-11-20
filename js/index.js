085b51362b23adc423bce13f98295910fb7e3092
enrique <german.junior93@gmail.com>
2020-11-20 18:21:49 -0400

    Intentando conectar con la API de marvel

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const URL_API = "https://gateway.marvel.com:443/v1/public/characters?name=Spider-Man&apikey=3bb70d706a9730be29cb1c76f9df1c1c";
+
+let http = new XMLHttpRequest();
+
+http.open("GET", URL_API, true);
+
+http.onreadystatechange = () => {
+    if (http.readyState === 4) {
+        if(http.status === 200) {
+            console.log("Datos obtenidos");
+        }
+        else {
+            console.log("Error al obtener los datos");
+        }
+    }
+}
+
+http.send();
