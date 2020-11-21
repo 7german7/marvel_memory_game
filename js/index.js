d7b2396e2b41f043f792a5bd082a7ef64f06473f
enrique <german.junior93@gmail.com>
2020-11-21 18:09:38 -0400

    update: obtener 10 caracteres

--- js/index.js
+++ js/index.js
@@ -8,21 +8,18 @@ const KEY_API = "&apikey=3bb70d706a9730be29cb1c76f9df1c1c";
 const HASH = "&hash=c8347048ae8c9d6a58495c461d50b09b";
 const BASE_URL = "https://gateway.marvel.com:443/v1/public/characters?name=";
 
-let personName = "Hulk";
-/*
-1. Spider-Man
-2. Thor
-3. Hulk
-4. Thanos
-5. Black Widow
-6. Captain America
-7. Black Panther
-8. Iron Man
-9. Hawkeye
-10. Loki
-*/
-
-let URL_API = `${BASE_URL}${personName}${TS}${KEY_API}${HASH}`;
+let characters = [
+    "Hulk",
+    "Spider-Man",
+    "Thor",
+    "Thanos",
+    "Black Widow",
+    "Captain America",
+    "Black Panther",
+    "Iron Man",
+    "Hawkeye",
+    "Loki"
+];
 
 let fetchData = (URL_API) => {
     return new Promise( (resolve, reject)=> {
@@ -43,14 +40,19 @@ let fetchData = (URL_API) => {
     });
 }
 
-fetchData(URL_API)
-    .then(response => {
-        console.log(response.data.results[0]);
-        let imgUrl = response.data.results[0].thumbnail.path;
-        let extension = response.data.results[0].thumbnail.extension;
+characters.forEach(character => {
+    let URL_API = `${BASE_URL}${character}${TS}${KEY_API}${HASH}`;
+    fetchData(URL_API)
+    .then(characterInfo => {
+        //console.log(characterInfo.data.results[0]);
+        let name = characterInfo.data.results[0].name;
+        let imgUrl = characterInfo.data.results[0].thumbnail.path;
+        let extension = characterInfo.data.results[0].thumbnail.extension;
+        console.log(name);
         console.log(imgUrl+"."+extension);
-        console.log(response.data.results[0].description);
+        console.log(characterInfo.data.results[0].description);
     })
     .catch(error =>{
         console.error(error);
     });
+});
