56ac99cc1c099c6ed81353cc04d38e9ae1c2455e
enrique <german.junior93@gmail.com>
2020-11-21 18:48:23 -0400

    update: se obtienen los 10 personajes mediante una lista de nombres y cuando la lista tiene 10 esta full se muestran los datos

--- js/index.js
+++ js/index.js
@@ -8,7 +8,8 @@ const KEY_API = "&apikey=3bb70d706a9730be29cb1c76f9df1c1c";
 const HASH = "&hash=c8347048ae8c9d6a58495c461d50b09b";
 const BASE_URL = "https://gateway.marvel.com:443/v1/public/characters?name=";
 
-let characters = [
+let characters = [];
+let charactersList = [
     "Hulk",
     "Spider-Man",
     "Thor",
@@ -40,17 +41,23 @@ let fetchData = (URL_API) => {
     });
 }
 
-characters.forEach(character => {
+charactersList.forEach(character => {
     let URL_API = `${BASE_URL}${character}${TS}${KEY_API}${HASH}`;
     fetchData(URL_API)
     .then(characterInfo => {
-        //console.log(characterInfo.data.results[0]);
-        let name = characterInfo.data.results[0].name;
-        let imgUrl = characterInfo.data.results[0].thumbnail.path;
-        let extension = characterInfo.data.results[0].thumbnail.extension;
-        console.log(name);
-        console.log(imgUrl+"."+extension);
-        console.log(characterInfo.data.results[0].description);
+        return characterInfo.data.results[0];
+    })
+    .then(characterInfo =>{
+        let name = characterInfo.name;
+        let imgUrl = characterInfo.thumbnail.path + characterInfo.thumbnail.extension;
+        characters.push({
+            "name": name,
+            "imgUrl": imgUrl
+        });
+        if(character.length===10){
+            console.log(characters);
+
+        };
     })
     .catch(error =>{
         console.error(error);
