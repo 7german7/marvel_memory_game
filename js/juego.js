824aed2dfc0a65abc8cba43e501c8f7cef83c1e8
enrique <german.junior93@gmail.com>
2020-11-21 22:07:45 -0400

    se creo la aplicacion utilizando POO

---
+++ js/juego.js
@@ -0,0 +1,85 @@
+/*Configurations*/
+const TS = "&ts=1";
+const KEY_API = "&apikey=3bb70d706a9730be29cb1c76f9df1c1c";
+const HASH = "&hash=c8347048ae8c9d6a58495c461d50b09b";
+const BASE_URL = "https://gateway.marvel.com:443/v1/public/characters?name=";
+const charactersList = [
+    "Hulk",
+    "Spider-Man",
+    "Thor",
+    "Thanos",
+    "Black Widow",
+    "Captain America",
+    "Black Panther",
+    "Iron Man",
+    "Hawkeye",
+    "Loki"
+];
+
+let URL_API = `${BASE_URL}${charactersList}${TS}${KEY_API}${HASH}`;
+
+class Game {
+    constructor(characters) {
+        this.charactersNames = characters;
+        this.characters = [];
+        this.resultsPromises = [];
+
+        this.getCharactersData();
+        Promise.all(this.resultsPromises).then(characters => {
+            this.characters = characters;
+            this.start();
+        });
+    }
+
+    start() {
+        this.showCharacters();
+        this.createCards();
+    }
+
+    getCharacters() {
+        return this.characters;
+    }
+
+    showCharacters() {
+        console.table(this.characters);
+    }
+
+    createCards() {
+        for (let i = 0; i < this.characters.length; i++) {
+            const character = this.characters[i].name;
+            let div = document.createElement("div");
+            div.innerHTML = character;
+            document.body.appendChild(div);
+        }
+    }
+
+    fetchCharacterData = (character) => {
+        let URL_API = `${BASE_URL}${character}${TS}${KEY_API}${HASH}`;
+        return new Promise( (resolve, reject)=> {
+            let http = new XMLHttpRequest();
+            http.open("GET", URL_API, true);
+            http.onreadystatechange = () => {
+                if (http.readyState === 4) {
+                    if(http.status === 200) {
+                        console.log("Datos obtenidos");
+                        let response = JSON.parse(http.responseText);
+                        resolve(response.data.results[0]);
+                    }
+                    else {
+                        reject(new Error("Error al obtener los datos" + URL_API));
+                    }
+                }
+            }
+            http.send();
+        });
+    }
+
+    getCharactersData = async () => {
+        await this.charactersNames.forEach(character => {
+            this.character = this.fetchCharacterData(character);
+            this.resultsPromises.push(this.character);
+        });
+    }
+}
+
+let game = new Game(charactersList);
